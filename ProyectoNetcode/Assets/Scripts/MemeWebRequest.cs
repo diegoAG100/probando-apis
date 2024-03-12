b52a89b28dd6255fbb7e38639503a54d2d4984ea
diegoAG100 <diegoamenedogonzalez01@gmail.com>
2024-03-12 21:36:28 +0100

    Ya esta creada la clase que guarda el json y se pueden ver las imagenes en una Image de unity

--- ProyectoNetcode/Assets/Scripts/MemeWebRequest.cs
+++ ProyectoNetcode/Assets/Scripts/MemeWebRequest.cs
@@ -1,15 +1,21 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class MemeWebRequest : MonoBehaviour
 {
 
+    Meme meme1;
+    Meme meme2;
+
+    public Image _EventTexture;
+
     void Start()
     {
         // https://imgflip.com/api
-        StartCoroutine(GetRequest("https://api.imgflip.com/get_memes"));
+        StartCoroutine(GetRequest("https://meme-api.com/gimme"));
 
     }
 
@@ -34,8 +40,55 @@ public class MemeWebRequest : MonoBehaviour
                     break;
                 case UnityWebRequest.Result.Success:
                     Debug.Log(webRequest.downloadHandler.text);
+
+                    JsonToResult(webRequest.downloadHandler.text);
+
+                    StartCoroutine(GetTexture());
+
                     break;
             }
         }
     }
+
+    private void JsonToResult(string json){
+        meme1 = JsonUtility.FromJson<Meme>(json);
+        Debug.Log(meme1.url);
+    }
+
+    IEnumerator GetTexture()
+    {
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(meme1.url);
+
+        yield return www.SendWebRequest();
+
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Texture2D myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+            
+            //Sprite.Create(myTexture,new Rect(0,0,myTexture.width,myTexture.height),Vector2.zero)
+
+            var material = new Material(Shader.Find("UI/Default"));
+            material.mainTexture = myTexture;
+
+            _EventTexture.material = material;
+        }
+    }
+
+}
+
+[Serializable] public class Meme{
+    public string postLink;
+    public string subreddit;
+    public string title;
+    public string url;
+    public string nsfw;
+    public string spoiler; 
+    public string author;
+    public string ups;
+    public string[] preview;
 }
